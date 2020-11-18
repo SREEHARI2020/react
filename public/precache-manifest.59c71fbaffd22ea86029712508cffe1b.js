b9c606c9805dccc34c19fe65467e99798f7ea9d1
SREEHARI <sreehari2610@gmail.com>
2020-11-18 12:33:15 +0530

    React project

---
+++ public/precache-manifest.59c71fbaffd22ea86029712508cffe1b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf346bebecf46ad1228932a81777d5a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98e006314df82f160bfa",
+    "url": "/static/css/2.7f0a60d8.chunk.css"
+  },
+  {
+    "revision": "baa7b25cfd3754703562",
+    "url": "/static/css/main.0262b00f.chunk.css"
+  },
+  {
+    "revision": "98e006314df82f160bfa",
+    "url": "/static/js/2.880091cb.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.880091cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "baa7b25cfd3754703562",
+    "url": "/static/js/main.f568223e.chunk.js"
+  },
+  {
+    "revision": "aff20233fc33123ab008",
+    "url": "/static/js/runtime-main.74f98a6f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
